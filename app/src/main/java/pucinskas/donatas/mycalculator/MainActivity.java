0e6b98dc2c6049ca0e3d39aa7660869dd8c9c1f5
Donatas <donciuxxs@gmail.com>
2018-04-12 20:42:02 +0300

    First commit.

---
+++ app/src/main/java/pucinskas/donatas/mycalculator/MainActivity.java
@@ -0,0 +1,30 @@
+package pucinskas.donatas.mycalculator;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void onClickNum(View view){
+
+    }
+
+    public void onClickSymbol(View view){
+
+    }
+
+    public void clearScr(View view){
+
+    }
+
+    public void printResult(View view){
+
+    }
+}
